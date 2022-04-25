45d9f05902c72c0f1c6f9bd1e066802e9e3ef9b7
JP-Ferrei <jpfdc9@gmail.com>
2022-04-25 19:22:15 -0300

    refactor: adding the args feature, behind schedule

--- alfaTeste/Program.cs
+++ alfaTeste/Program.cs
@@ -1,10 +1,9 @@
 ﻿using alfaTeste;
 
-Console.WriteLine("Enter the path to the file");
-
-var path = Console.ReadLine();
+var path = args.Length > 0 ? args[0]: null;
 while (string.IsNullOrEmpty(path))
 {
+    Console.WriteLine("Enter the path to the file");
     path = Console.ReadLine();
 }
 var logIO = new LogIO(path, "logs.txt");
@@ -14,7 +13,6 @@ if (!logIO.IsRunnable)
     Console.WriteLine("wait 1 minute to run this application again");
     Thread.Sleep(5000);
     Environment.Exit(0);
-
 }
 await logIO.GetUserInformation();
 
